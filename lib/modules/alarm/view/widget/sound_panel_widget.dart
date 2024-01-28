d77011230037e881736fbaa01650763cc281d2b5
홍순원 <sunwonsw95@naver.com>
2024-01-28 10:16:24 +0900

    feat:implements pre-listen sound asset

--- lib/modules/alarm/view/widget/sound_panel_widget.dart
+++ lib/modules/alarm/view/widget/sound_panel_widget.dart
@@ -1,7 +1,9 @@
+import 'package:alarm/alarm.dart';
 import 'package:easy_alarm/common/asset_path.dart';
 import 'package:easy_alarm/style/colors.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class SoundPanelWidget extends StatefulWidget {
   const SoundPanelWidget({
@@ -16,10 +18,18 @@ class SoundPanelWidget extends StatefulWidget {
 }
 
 class _SoundPanelWidgetState extends State<SoundPanelWidget> {
+  late final AudioPlayer _player;
+
   final TextStyle _labelTextStyle =
       const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: CustomColors.black);
 
   SoundAssetPath _selectedSound = SoundAssetPath.defaultSound;
+
+  @override
+  void initState() {
+    _player = AudioPlayer();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,65 +38,75 @@ class _SoundPanelWidgetState extends State<SoundPanelWidget> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            Text("alarm.soundLabel".tr(), style: _labelTextStyle),
             Expanded(
-              flex: 7,
-              child: Text("alarm.soundLabel".tr(), style: _labelTextStyle),
-            ),
-            // Expanded(
-            //   flex: 4,
-            //   child: DecoratedBox(
-            //     decoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(20.0),
-            //       border: Border.all(color: CustomColors.grey40, width: 2.0),
-            //     ),
-            //     child: DropdownButton(
-            //       value: _selectedSound,
-            //       isExpanded: true,
-            //       isDense: true,
-            //       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-            //       underline: const Offstage(),
-            //       onChanged: (value) {
-            //         setState(() => _selectedSound = value as SoundAssetPath);
-            //         widget.onSelectSound(_selectedSound);
-            //       },
-            //       items: SoundAssetPath.values
-            //           .map(
-            //             (e) => DropdownMenuItem(
-            //               value: e,
-            //               child: Text(e.name),
-            //             ),
-            //           )
-            //           .toList(),
-            //     ),
-            //   ),
-            // ),
-            Expanded(
-              flex: 4,
-              child: DropdownMenu(
-                initialSelection: _selectedSound,
-                requestFocusOnTap: false,
-                enableSearch: false,
-                enableFilter: false,
-                inputDecorationTheme: InputDecorationTheme(
-                  isDense: true,
-                  isCollapsed: true,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                    borderSide: BorderSide(color: CustomColors.grey40, width: 2.0),
+              flex: 5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Visibility.maintain(
+                    visible: _player.playing,
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _player.stop();
+                        });
+                      },
+                      icon: const Icon(Icons.stop),
+                    ),
                   ),
-                ),
-                onSelected: (value) {
-                  setState(() => _selectedSound = value as SoundAssetPath);
-                  widget.onSelectSound(_selectedSound);
-                },
-                dropdownMenuEntries: SoundAssetPath.values
-                    .map(
-                      (e) => DropdownMenuEntry(
-                        value: e,
-                        label: e.name,
+                  DropdownMenu(
+                    initialSelection: _selectedSound,
+                    requestFocusOnTap: false,
+                    enableSearch: false,
+                    enableFilter: false,
+                    menuStyle: MenuStyle(
+                      backgroundColor: MaterialStateColor.resolveWith((states) {
+                        return CustomColors.white;
+                      }),
+                      elevation: MaterialStateProperty.resolveWith((states) => 5.0),
+                      shape: MaterialStateProperty.resolveWith(
+                        (states) => RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
                       ),
-                    )
-                    .toList(),
+                    ),
+                    inputDecorationTheme: InputDecorationTheme(
+                      isDense: true,
+                      isCollapsed: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(color: CustomColors.grey40, width: 2.0),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    ),
+                    onSelected: (value) async {
+                      if (value == null) return;
+                      if (_player.playing) await _player.stop();
+
+                      await _player.setAsset(value.path);
+
+                      setState(() {
+                        _selectedSound = value;
+                        _player.play();
+                      });
+                      widget.onSelectSound(_selectedSound);
+                    },
+                    dropdownMenuEntries: SoundAssetPath.values
+                        .map(
+                          (e) => DropdownMenuEntry(
+                            value: e,
+                            label: e.name,
+                            style: ButtonStyle(
+                              textStyle: MaterialStateProperty.resolveWith(
+                                (states) => const TextStyle(fontSize: 16.0),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ],
               ),
             ),
           ],
