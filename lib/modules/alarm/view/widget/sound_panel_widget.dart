1566b4e0ba05ca1da94e6c0c8f4f286482b20519
홍순원 <sunwonsw95@naver.com>
2024-01-28 09:51:10 +0900

    feat:refactor sound panel

--- lib/modules/alarm/view/widget/sound_panel_widget.dart
+++ lib/modules/alarm/view/widget/sound_panel_widget.dart
@@ -32,20 +32,58 @@ class _SoundPanelWidgetState extends State<SoundPanelWidget> {
               flex: 7,
               child: Text("alarm.soundLabel".tr(), style: _labelTextStyle),
             ),
+            // Expanded(
+            //   flex: 4,
+            //   child: DecoratedBox(
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(20.0),
+            //       border: Border.all(color: CustomColors.grey40, width: 2.0),
+            //     ),
+            //     child: DropdownButton(
+            //       value: _selectedSound,
+            //       isExpanded: true,
+            //       isDense: true,
+            //       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            //       underline: const Offstage(),
+            //       onChanged: (value) {
+            //         setState(() => _selectedSound = value as SoundAssetPath);
+            //         widget.onSelectSound(_selectedSound);
+            //       },
+            //       items: SoundAssetPath.values
+            //           .map(
+            //             (e) => DropdownMenuItem(
+            //               value: e,
+            //               child: Text(e.name),
+            //             ),
+            //           )
+            //           .toList(),
+            //     ),
+            //   ),
+            // ),
             Expanded(
               flex: 4,
-              child: DropdownButton(
-                value: _selectedSound,
-                isExpanded: true,
-                onChanged: (value) {
+              child: DropdownMenu(
+                initialSelection: _selectedSound,
+                requestFocusOnTap: false,
+                enableSearch: false,
+                enableFilter: false,
+                inputDecorationTheme: InputDecorationTheme(
+                  isDense: true,
+                  isCollapsed: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(color: CustomColors.grey40, width: 2.0),
+                  ),
+                ),
+                onSelected: (value) {
                   setState(() => _selectedSound = value as SoundAssetPath);
                   widget.onSelectSound(_selectedSound);
                 },
-                items: SoundAssetPath.values
+                dropdownMenuEntries: SoundAssetPath.values
                     .map(
-                      (e) => DropdownMenuItem(
+                      (e) => DropdownMenuEntry(
                         value: e,
-                        child: Text(e.name),
+                        label: e.name,
                       ),
                     )
                     .toList(),
