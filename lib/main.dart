12282970dc0878adc61936ee576b7703e9425f60
홍순원 <sunwonsw95@naver.com>
2024-02-13 20:05:05 +0900

    feat:add firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:easy_alarm/core/alarm_manager.dart';
 import 'package:easy_alarm/modules/main/view/page/my_app.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   await Future.wait([
     EasyLocalization.ensureInitialized(),
